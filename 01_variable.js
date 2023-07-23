5ca6e45a0e2cf5dcb686088a7f97f2ad43cb9a07
Pratibhajadhav <pratibhaips285@gmail.com>
2023-07-23 16:37:56 +0530

    variable.js name
    variable.js name

---
+++ 01_variable.js
@@ -0,0 +1,9 @@
+var myFirstName;
+myFirstName = "pratibha";
+console.log(myFirstName);
+
+var city = "pune";
+console.log(city);
+
+var pin;
+console.log(pin);
---
+++ 02_variablefullname.js
@@ -0,0 +1,35 @@
+
+console.log("========step 1=======");
+var myFullName="abc xyz";
+console.log(myFullName);
+myFullName="abc pqr xyz";
+console.log(myFullName);
+console.log("========step 2=======");
+var myLove="l love js";
+console.log(myLove);
+console.log("========step 3=======");
+var firstName="First Name is-Pratibha";
+console.log(firstName);
+
+var lastName="Last Name is-Jadhav";
+console.log(lastName);
+
+var age="Age- 28";
+console.log(age);
+
+var address="address-Jath";
+console.log(address);
+
+var pincode=416410;
+console.log(pincode);
+
+var gender=" Gender - F";
+console.log(gender);
+
+var isMarried="is married- true";
+console.log(isMarried);
+
+
+
+
+
